fd1f69a44665442dea278670bb33bc7b11fc960a
Muhammad Zainu Iqbal <16.9309@stis.ac.id>
2018-12-31 12:32:41 +0700

    Update Server_Model.php

--- application/models/Server_Model.php
+++ application/models/Server_Model.php
@@ -353,19 +353,24 @@ class Server_Model extends CI_Model {
 	}
 
 	function get_detail_listing(){
-		$que = $this->db->query("
-		SELECT t1.nim, t1.kode_bs, t1.kode_desa, t1.nama_desa, t1.kode_kecamatan, t1.nama_kecamatan, t1.kode_kabupaten, t1.nama_kabupaten, t2.jumlah
-		FROM
-		(SELECT dkb.id as kode_bs, dkb.nama as nama_bs, dkd.id as kode_desa, dkd.nama as nama_desa, dkc.id as kode_kecamatan, dkc.nama as nama_kecamatan, dkk.id as kode_kabupaten, dkk.nama as nama_kabupaten, dkb.nim
-		FROM dummy_kode_bloksensus dkb
-		INNER JOIN dummy_kode_kelurahandesa dkd ON dkd.id = dkb.kelurahandesa AND dkd.kecamatan = dkb.kecamatan AND dkd.kabupaten = dkb.kabupaten
-		INNER JOIN dummy_kode_kecamatan dkc ON dkc.id = dkb.kecamatan AND dkc.kabupaten = dkb.kabupaten
-		INNER JOIN dummy_kode_kabupaten dkk ON dkk.id = dkb.kabupaten WHERE dkk.id <> '99' ) t1
-		LEFT OUTER JOIN
-		(SELECT COUNT(*) as jumlah, kodeBs FROM backup_datart GROUP BY kodeBs ) t2
-		ON t1.kode_bs = t2.kodeBs");
+		// $que = $this->db->query("
+		// SELECT t1.nim, t1.kode_bs, t1.kode_desa, t1.nama_desa, t1.kode_kecamatan, t1.nama_kecamatan, t1.kode_kabupaten, t1.nama_kabupaten, t2.jumlah
+		// FROM
+		// (SELECT dkb.id as kode_bs, dkb.nama as nama_bs, dkd.id as kode_desa, dkd.nama as nama_desa, dkc.id as kode_kecamatan, dkc.nama as nama_kecamatan, dkk.id as kode_kabupaten, dkk.nama as nama_kabupaten, dkb.nim
+		// FROM dummy_kode_bloksensus dkb
+		// INNER JOIN dummy_kode_kelurahandesa dkd ON dkd.id = dkb.kelurahandesa AND dkd.kecamatan = dkb.kecamatan AND dkd.kabupaten = dkb.kabupaten
+		// INNER JOIN dummy_kode_kecamatan dkc ON dkc.id = dkb.kecamatan AND dkc.kabupaten = dkb.kabupaten
+		// INNER JOIN dummy_kode_kabupaten dkk ON dkk.id = dkb.kabupaten WHERE dkk.id <> '99' ) t1
+		// LEFT OUTER JOIN
+		// (SELECT COUNT(*) as jumlah, kodeBs FROM backup_datart GROUP BY kodeBs ) t2
+		// ON t1.kode_bs = t2.kodeBs");
 
-		return $que->result();
+		// return $que->result();
+
+		$que1 = $this->load->database('pkl58_monitoring', TRUE)->query("
+		SELECT t1.nim, t1.kode_bs, t1.kode_desa, t1.nama_desa, t1.kode_kecamatan, t1.nama_kecamatan, t1.kode_kabupaten, t1.nama_kabupaten, t1.jumlah
+		FROM detail_listing t1
+		");
 	}
 
 	function get_agregat_listing() {
