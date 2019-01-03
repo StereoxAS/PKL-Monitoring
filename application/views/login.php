321c88fd71313e00f6d0c8d2ca9850fa448b3076
mujezaa <mujezaa@gmail.com>
2019-01-03 23:48:45 +0700

    Login

--- application/views/login.php
+++ application/views/login.php
@@ -3,11 +3,11 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=1,initial-scale=1,user-scalable=1" />
-	<title>Insert title here</title>
-	
+	<title>Web Monitoring PKL 58</title>
+	<link rel="icon" href="<?php echo base_url();?>resources/images/icon2.png" class="img-circle" type="image/ico"/>
 	<link href="http://fonts.googleapis.com/css?family=Lato:100italic,100,300italic,300,400italic,400,700italic,700,900italic,900" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>resources/vendor/bootstrap/css/bootstrap.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>resources/css/login.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>resources/css/login.css" /> 
 	
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -21,14 +21,14 @@
 	    <section class="login-form">
 		
 		<section>
-			<img src="<?php echo base_url();?>resources/images/logodepan.png" alt="" style="width: 250px" />
+			<img src="<?php echo base_url();?>resources/images/logodepan5.png" alt="" style="width: 330px" />
 		</section>
 		
 		<div class="panel panel-default">
 		  	<div class="panel-body">
 		    	<?php echo form_open('login/verification', array('class' => 'form-horizontal col-md-12 text-center')); ?>
 					<div class="form-group">
-						    <?php echo form_input(array('id' => 'nim', 'name' => 'nim', 'class' => 'form-control', 'placeholder' => 'Masukkan NIM / Email (untuk dosen)')); ?>
+						    <?php echo form_input(array('id' => 'nim', 'name' => 'nim', 'class' => 'form-control', 'placeholder' => 'Masukkan NIM / Email')); ?>
 						    </div>
 					
 					
@@ -48,6 +48,6 @@
 	</section>
 	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script> <!-- assets/bootstrap/js/bootstrap.min.js -->
 </body>
 </html>
